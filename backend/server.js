19297565731ab51d8fc08ed5e641f89aea07a3c0
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-17 23:00:23 +0100

    Added routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const blogRoutes = require('./routes/blog');
 
 //app
 const app = express()
@@ -21,12 +22,9 @@ app.use(cookieParser())
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({ origin: `${process.env.CLIENT_URL}`}));
 }
+// routes middleware
+app.use('/api', blogRoutes);
 
-
-// routes
-app.get('/api', (req, res) => {
-    res.json({time: Date().toString()})
-})
 
 // port
 const port = process.env.PORT || 8000
