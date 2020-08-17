c23676f722c1fc483d061f846bb35caac328e343
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-17 16:20:35 +0100

    Server configurations added

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 //cors
-app.use(cors())
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}`}));
+}
+
 
 // routes
 app.get('/api', (req, res) => {
