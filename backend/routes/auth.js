38cc7d33a0d22afe3ea745681ef66e86d16bfa49
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-18 18:16:48 +0100

    Added auth validators

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {signup} = require('../controlers/auth')
+const {signup} = require('../controlers/auth');
 
-router.post('/signup', signup)
+// validators
+const {runValidation} = require('../validators');
+const {userSignupValidator} = require('../validators/auth');
+
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
