31e1577c44774d1e3eb60e8fbd02fdd9752c54c9
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-18 16:51:59 +0100

    Added authentication routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
 //app
 const app = express()
@@ -24,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 // routes middleware
 app.use('/api', blogRoutes);
-
+app.use('/api', authRoutes);
 
 // port
 const port = process.env.PORT || 8000
