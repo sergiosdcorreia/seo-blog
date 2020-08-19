a3a2867d55e0485234ae7f632594c215aa20e39e
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-19 23:12:46 +0100

    Added token to access private pages

--- backend/controlers/auth.js
+++ backend/controlers/auth.js
@@ -61,3 +61,16 @@ exports.signin = (req, res) => {
         });
     });
 }
+
+exports.signout = (req, res) => {
+    res.clearCookie('token');
+    res.json({
+        message: 'Signout success'
+    });
+};
+
+// middleware
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['RS256']
+});
