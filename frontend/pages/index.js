3e6bdd4056af131246744f73d228965c0337561e
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-20 14:47:29 +0100

    Started frontend setup. Created Layout component.

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const Index = () => {
+    return (
+        <Layout>
+            <h2>Index Page</h2>
+        </Layout>
+    );
+};
+ 
+
+export default Index;
