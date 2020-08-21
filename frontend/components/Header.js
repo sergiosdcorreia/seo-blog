7debdfd4a6045b5ab06d215c7176204771544b0e
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-21 12:04:55 +0100

    Added links to pages with next/link

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,16 +1,25 @@
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-
+import Link from 'next/link';
 import { APP_NAME } from '../config';
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">{APP_NAME}</Navbar.Brand>
+        <Navbar.Brand>
+            <Link href="/">
+                <a>{APP_NAME}</a>
+            </Link>
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Link href="/signin">
+                    <a>Signin</a>
+                </Link>        
+                <Link href="/signup">
+                    <a>Signup</a>
+                </Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
---
+++ frontend/pages/signin.js
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const Signin = () => {
+    return (
+        <Layout>
+            <h2>Signin page</h2>
+            
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </Layout>
+    )
+}
+
+export default Signin;
