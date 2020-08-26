27770e02c91027f2e9e27da6068d84ae9551846f
Sergio Correia <sergiosdcorreia@gmail.com>
2020-08-26 18:04:17 +0100

    Signout and conditionally showing nav links

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,8 @@
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import Link from 'next/link';
 import { APP_NAME } from '../config';
+import { signout, isAuth } from '../actions/auth';
+import { Router } from 'next/router';
 
 const Header = () => {
     return (
@@ -14,19 +16,28 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-                <Link href="/signin">
-                    <a>Signin</a>
-                </Link>        
-                <Link href="/signup">
-                    <a>Signup</a>
-                </Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+                {!isAuth && 
+                <>
+                    <Link href="/signin">
+                        <a>Signin</a>
+                    </Link>        
+                    <Link href="/signup">
+                        <a>Signup</a>
+                    </Link>
+                </>
+                }
+                
+                {isAuth() && (
+                    <a onClick={() => signout(() => Router.replace(`/signin`))}>Signout</a>
+                )}
+
+                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
             </Nav>
             <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
