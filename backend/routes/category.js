c070add6d2e9365bb81b1342f81225d8ace033ac
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-01 03:11:51 +0100

    Added category list, read and delete.

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controlers/category');
+const { create, list, read, remove } = require('../controlers/category');
 const { requireSignin, adminMiddleware } = require('../controlers/auth');
 const { runValidation } = require('../validators');
 const { categoryCreateValidator } = require('../validators/category');
 
 
 router.post('/category', categoryCreateValidator, runValidation, requireSignin, adminMiddleware, create);
+router.get('/categories', list);
+router.get('/category/:slug', read);
+router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
 
 module.exports = router;
