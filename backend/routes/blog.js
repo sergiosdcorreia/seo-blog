e2aa2dae8cec34cb41171a8a4121a23c27a2edbe
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-05 03:35:59 +0100

    Creating blog with photo.

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {time} = require('../controlers/blog')
+const { create } = require('../controlers/blog');
+const { requireSignin, adminMiddleware} = require('../controlers/auth');
 
-router.get('/', time)
+router.post('/blog', requireSignin, adminMiddleware, create)
 
 module.exports = router;
