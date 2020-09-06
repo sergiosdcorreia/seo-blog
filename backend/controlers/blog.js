29d62bac06ceecb36ac2ecff2704a61e7a46ab7a
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-06 17:08:28 +0100

    Generated blog excerpt

--- backend/controlers/blog.js
+++ backend/controlers/blog.js
@@ -7,6 +7,7 @@ const Category = require('../models/category');
 const Tag = require('../models/tag');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 const fs = require('fs');
+const { smartTrim } = require('../helpers/blog');
 
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm()
@@ -45,11 +46,12 @@ exports.create = (req, res) => {
         };
 
         let blog = new Blog();
-        blog.title = title
-        blog.body = body
-        blog.slug = slugify(title).toLowerCase()
-        blog.mtitle = `${title} | ${process.env.APP_NAME}`
-        blog.mdesc = stripHtml(body.substring(0, 160))
+        blog.title = title;
+        blog.body = body;
+        blog.excerpt = smartTrim(body, 320, ' ', ' ...');
+        blog.slug = slugify(title).toLowerCase();
+        blog.mtitle = `${title} | ${process.env.APP_NAME}`;
+        blog.mdesc = stripHtml(body.substring(0, 160));
         blog.postedBy = req.user._id;
 
         // categories and tags
