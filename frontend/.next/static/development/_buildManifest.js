7e0d10c3b0f6fef4fdb7e89775604e98dba7babc
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-06 17:54:27 +0100

    Create blog actions and imports

--- frontend/.next/static/development/_buildManifest.js
+++ frontend/.next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/admin/crud/category-tag":["static\u002Fchunks\u002Fpages\u002Fadmin\u002Fcrud\u002Fcategory-tag.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
