43b71cbf03083abf42e1e0d421397b5649948aa5
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-08 20:54:17 +0100

    List all blogs, categories and tags.

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controlers/blog');
+const { create, list, listAllBlogsCategoriesTags, read, remove, update } = require('../controlers/blog');
 const { requireSignin, adminMiddleware} = require('../controlers/auth');
 
-router.post('/blog', requireSignin, adminMiddleware, create)
+router.post('/blog', requireSignin, adminMiddleware, create);
+router.get('/list');
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
+router.put('/blog/:slug', requireSignin, adminMiddleware, update);
 
 module.exports = router;
