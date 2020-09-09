ef11ff33dd37d10222a21dc2767f5292cd9d376d
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-09 19:42:35 +0100

    Backend added blog photo.

--- backend/controlers/blog.js
+++ backend/controlers/blog.js
@@ -250,3 +250,18 @@ exports.update = (req, res) => {
         })
     })
 };
+
+exports.photo = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+    Blog.findOne({ slug })
+    .select('photo')
+    .exec((err, slug) => {
+        if(err || !blog) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.set('Content-Type', blog.photo.contentType);
+        return res.send(blog.photo.data);
+    });
+};
