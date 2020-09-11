e2f8a34d8ebf86ce5572338184a09803608382dd
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-11 11:56:38 +0100

    Single blog with fullwidth image and content.

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -1,19 +1,70 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { withRouter } from 'next/router';
 import Layout from '../../components/Layout';
+import moment from 'moment';
+import renderHTML from 'react-render-html';
 import { useState } from 'react';
 import { singleBlog } from '../../actions/blog';
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config'; 
 
-const SingleBlog = ({ blog, router }) => {
+const SingleBlog = ({ blog }) => {
+    const showBlogCategories = blog => {
+        return blog.categories.map((c, i) => {
+            <Link key={i} href={`/categories/${c.slug}`}>
+                <a className="btn btn-outline-primary mr-1 ml-1 mt-3">{c.name}</a>
+            </Link>
+        })
+    };
+
+    const showBlogTags = blog => {
+        return blog.tags.map((t, i) => {
+            <Link key={i} href={`/tags/${t.slug}`}>
+                <a className="btn btn-outline-primary mr-1 ml-1 mt-3">{t.name}</a>
+            </Link>
+        })
+    };
+
     return (
         <>
             <Layout>
                 <main>
                     <article>
                         <div className="container-fluid">
-                            <section>{JSON.stringify(blog)}</section>
+                            <section>
+                                <div className="row" style{{ marginTop: '-30px' }}>
+                                    <img src={`${API}/blog/photo/${blog.slug}`} alt={blog.title} className="img img-fluid featured-image" />
+                                </div>
+                            </section>
+                            <section>
+                                <p className="lead mt-3 mark">
+                                Written by {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
+                                </p>
+                                <div className="pb-3">
+                                    {showBlogCategories(blog)}
+                                    {showBlogTags(blog)}
+                                    <br/>
+                                    <br/>
+                                </div>
+                            </section>
+                        </div>
+
+                        <div className="container">
+                            <section>
+                                <div className="col-md-12 lead">
+                                    {renderHTML(blog.body)}
+                                </div>
+                            </section>
+
+                        </div>
+
+                        <div className="container pb-5">
+                            <h4 className="text-center pt-5 pb-5 h2">Related blogs</h4>
+                            <hr />
+                            <p>Show related blogs</p>
+                        </div>
+
+                        <div className="container pb-5">
+                            <p>Show comments</p>
                         </div>
                     </article>
                 </main>
@@ -32,4 +83,4 @@ SingleBlog.getInitialProps = ({query}) => {
     })
 }
 
-export default withRouter(SingleBlog);
+export default SingleBlog;
