333c27f14abf2419d12bbcfeca3848ab3b42df31
Sergio Correia <sergiosdcorreia@gmail.com>
2020-09-18 04:18:05 +0100

    Added button for delete blogs

--- frontend/components/crud/BlogRead.js
+++ frontend/components/crud/BlogRead.js
@@ -1,13 +1,70 @@
 import { useState, useEffect } from 'react';
 import Router from 'next/router';
 import { getCookie, isAuth } from '../../actions/auth';
-
-import { list, removeblog } from '../../actions/blog';
+import moment from 'moment';
+import { list, removeblog, removeBlog } from '../../actions/blog';
 
 const BlogRead = () => {
+    const [blogs, setblogs] = useState([]);
+    const [message, setMessage] = useState('');
+    const token = getCookie('token');
+
+    useEffect(() => {
+        loadBlogs();
+    }, []);
+
+    const loadBlogs = () => {
+        list().then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setblogs(data);
+            }
+        });
+    };
+
+    const deleteBlog = (slug) => {
+        removeBlog(slug, token).then(data => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                setMessage(data.message);
+                loadBlogs();
+            }
+        });
+    };
+
+    const deleteConfirm = (slug) => {
+        let answer = window.confirm('Are you sure you want to delete your blog?');
+        if(answer) {
+            deleteBlog(slug);
+        };
+    };
+
+    const showAllBlogs = () => {
+        return blogs.map((blog, i) => {
+            return (
+                <div key={i} className="pb-5">
+                    <h3>{blog.title}</h3>
+                    <p className="mark">
+                        Written by {blog.postedBy.name} | Published on {moment(blog.updateAt).fromNow()}
+                    </p>
+                    <button className="btn btn-sm btn-danger" onClick={() => deleteConfirm(blog.slug)}>
+                        Delete
+                    </button>
+                </div>
+            )
+        })
+    }
+
     return(
         <>
-            <p>Update delete blogs</p>
+            <div className="row">
+                <div className="col-md-12">
+                    {message && <div className="alert alert-warning">{message}</div>}
+                    {showAllBlogs()}
+                </div>
+            </div> 
         </>
     )
 }
